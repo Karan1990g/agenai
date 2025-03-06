cc7c357be71b07f5ce346649209a8ce5a9881391
karanziie1990 <karanwithgcloud@gmail.com>
2025-03-06 21:19:55 +0530

    Ready for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',  // Ensures correct asset loading on Vercel
   plugins: [react()],
-  base: './',  // Ensures correct asset loading on Vercel
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
