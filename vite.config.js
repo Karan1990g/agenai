f575b1de97a79f4e24accc1bbaa5bcf0b2b691c0
karanziie1990 <karanwithgcloud@gmail.com>
2025-03-06 20:52:17 +0530

    Deployement on Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  server: {
-    port: 3000, // Used in development (optional)
+  build: {
+    outDir: 'dist', // Ensure the build output directory is 'dist'
   },
-  preview: {
-    port: 10000, // Ensure Render uses a fixed port
-    host: true,  // Bind to all network interfaces
-  }
 });
